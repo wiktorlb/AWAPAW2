d9954b3f03948fb3b6d8170ca04fd3de5824c606
wiktorlb <82231639+wiktorlb@users.noreply.github.com>
2024-11-04 23:09:15 +0100

    walidacja

--- jsf_01/src/main/java/com/jsfcourse/calc/CalcBB.java
+++ jsf_01/src/main/java/com/jsfcourse/calc/CalcBB.java
@@ -16,36 +16,36 @@ import jakarta.faces.context.FacesContext;
 @Named
 @RequestScoped
 //@SessionScoped
-public class CalcBB {
-	private String amount;
-	private String length;
-	private String perc;
+public class CalcBB{
+	private Double amount;
+	private Double length;
+	private Double perc;
 	private Double result;
 
 	@Inject //DI - Dependencies Injection
 	FacesContext ctx;
 	
-	public String getAmount() {
+	public Double getAmount() {
 		return amount;
 	}
 
-	public void setAmount(String amount) {
+	public void setAmount(Double amount) {
 		this.amount = amount;
 	}
 
-	public String getLength() {
+	public Double getLength() {
 		return length;
 	}
 
-	public void setLength(String length) {
+	public void setLength(Double length) {
 		this.length = length;
 	}
 
-	public String getPerc() {
+	public Double getPerc() {
 		return perc;
 	}
 
-	public void setPerc(String perc) {
+	public void setPerc(Double perc) {
 		this.perc = perc;
 	}
 
@@ -53,26 +53,12 @@ public class CalcBB {
 		return result;
 	}
 
-	public boolean Math() {
-		try {
-			double amount = Double.parseDouble(this.amount);
-			double length = Double.parseDouble(this.length);
-			double perc = Double.parseDouble(this.perc);
-
-			result = (amount + (amount * perc/100))/(length * 12);
-			
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
-			return true;
-		} catch (Exception e) {
-			ctx.addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
-			return false;
-		}
-	}
 	public String calc() {
-		if(Math()) {
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Miesieczna rata wynosi: " + result, null));
-		}
+		result = (amount + (amount * perc/100))/(length * 12);
+			
+		ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+		ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Miesieczna rata wynosi: " + result, null));
+		
 		return null;
 	}
 
